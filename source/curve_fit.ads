b9d11ccf59d64e56e98e894843ec15c66052b065
Maxim Reznik <reznikmm@gmail.com>
2024-04-07 10:36:11 +0300

    Add initial empty project

---
+++ source/curve_fit.ads
@@ -0,0 +1,8 @@
+--  SPDX-FileCopyrightText: 2024 Max Reznik <reznikmm@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+----------------------------------------------------------------
+
+package Curve_Fit is
+   pragma Pure;
+end Curve_Fit;
